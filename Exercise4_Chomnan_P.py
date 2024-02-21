71540b1c38e12fa85207e7a8b565a4383cff5adf
Chomnan441 <chomnan415@gmail.com>
2024-02-21 20:19:14 +0700

    Create Exercise4_Chomnan_P.py

---
+++ Exercise4_Chomnan_P.py
@@ -0,0 +1,14 @@
+Firstname = "Chomnan"
+Lastname= "Phokhawatchanan"
+StudentID= "B5905549"
+FE =89.0 #Foundation English
+GB = 75.5 #General Business
+ICS = 85.0 #Introduction to Computer Sysytem
+CP = 87.9 #Computer Programming
+print(Firstname,Lastname,StudentID)
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Sysytem :",ICS)
+print("Computer Programming :",CP)
+
