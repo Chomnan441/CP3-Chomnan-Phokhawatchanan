b98732911b7e4ea50a7f3ff5fd9d6765378a62c9
Chomnan441 <chomnan415@gmail.com>
2024-02-26 01:22:05 +0700

    Create Exercise5_1_Chomnan_P.py

---
+++ Exercise5_1_Chomnan_P.py
@@ -0,0 +1,10 @@
+x = float(input("In put x value : "))
+y = float(input("In put y value : "))
+Result_xPlusy= x+y
+Result_xMinusy= x-y
+Result_xTimesy= x*y
+Result_xdividedBy_y= x/y
+print(x,"+",y,"=", Result_xPlusy)
+print(x,"-",y,"=", Result_xMinusy)
+print(x,"*",y,"=", Result_xTimesy)
+print(x,"/",y,"=", Result_xdividedBy_y)
