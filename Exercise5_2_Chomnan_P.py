7a37f88fa5b534e1213395474d08e6456ea895a7
Chomnan441 <chomnan415@gmail.com>
2024-02-26 02:05:31 +0700

    Update Exercise5_2_Chomnan_P.py

--- Exercise5_2_Chomnan_P.py
+++ Exercise5_2_Chomnan_P.py
@@ -1,5 +1,9 @@
 print("Calculate velocity(v)")
-s = float(input("Distance(km) : "))
-t = float(input("Time(hr) : "))
+s = float(input("Distance(km unit & at least 1 km) : "))
+if s < 1 :
+    s = 1
+t = float(input("Time(hr unit & at least 1 hr) : "))
+if t < 1 :
+    t = 1
 v = s/t
 print("Velocity =",v,"km/hr")
