000cbf29fd7bc7bbffc3394659640ed49a174928
Chomnan441 <chomnan415@gmail.com>
2024-03-17 04:13:11 +0700

    Create Lecture50_Chomnan_P.py

---
+++ Lecture50_Chomnan_P.py
@@ -0,0 +1,12 @@
+def xPlusy(x,y):
+    print(x+y)
+def xMinusy(x,y):
+    print(x-y)
+def xTimesy(x,y):
+    print(x*y)
+def xDevideByy(x,y):
+    print(x/y)
+xPlusy(10,5)
+xMinusy(10,5)
+xTimesy(10,5)
+xDevideByy(10,5)
