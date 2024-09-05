f68cd501e7ba61fa8a7b4436821340ca17bb7cef
Chomnan441 <chomnan415@gmail.com>
2024-09-06 03:40:47 +0700

    Update CurrencyConverter_Chomnan_P.py

--- CurrencyConverter_Chomnan_P.py
+++ CurrencyConverter_Chomnan_P.py
@@ -1,8 +1,13 @@
 from currency_converter import CurrencyConverter
-c = CurrencyConverter(fallback_on_missing_rate=True)
-
+from datetime import date
+#c = CurrencyConverter(fallback_on_missing_rate=True)
+c = CurrencyConverter(fallback_on_wrong_date=True)
 from tkinter import *
 from tkinter import ttk
+
+import datetime
+import statistics
+
 
 MainWindow = Tk()
 MainWindow.title("Currency converter")
@@ -33,7 +38,9 @@ label2.place(x=205, y=25)
 label3= Label(MainWindow,text="to")
 label3.place(x=185, y=50)
 label_last_available_date_currency = Label(MainWindow)
-label_last_available_date_currency.place(x=22, y=101)
+label_last_available_date_currency.place(x=22, y=102)
+label_average_currency_a_month = Label(MainWindow)
+label_average_currency_a_month.place(x=22, y=118)
 
 
 def search_currency(event):
@@ -66,13 +73,36 @@ def convert_click_button(event):
 
    result_exchange = c.convert(float(text_box_currency.get()), combo_currency.get(), combo_new_currency.get())
    print(result_exchange)
-   result_exchange = float("{:.2f}".format(result_exchange))
+   result_exchange = float("{:.3f}".format(result_exchange))
    lable_result.configure(text=result_exchange)
    first_date, last_date = c.bounds[combo_currency.get()]
    rate_currency = c.convert(1, combo_currency.get(), combo_new_currency.get())
-   last_update_currency = "1 " + str(combo_currency.get()) + " : " + str(float("{:.2f}".format(rate_currency))) + " " + str(combo_new_currency.get()) + ' last update on ' + str(last_date)
+   last_update_currency = "1 " + str(combo_currency.get()) + " : " + str(float("{:.3f}".format(rate_currency))) + " " + str(combo_new_currency.get()) + ' last update on ' + str(last_date)
    label_last_available_date_currency.configure(text=last_update_currency)
 
+
+   currency_count_list =[]
+   for count_date_ago in range(31):
+
+      # today_count = datetime.datetime.now()
+      today_count = datetime.date.today()
+      day_count = datetime.timedelta(days=count_date_ago + 1)
+      date_ago = today_count - day_count
+      # print(date_ago.day)
+      # print(date_ago.month)
+      # print(date_ago.year)
+
+      first_day_in_month = today_count.replace(day=1)
+      last_month = first_day_in_month - datetime.timedelta(days=1)
+      # print(last_month)
+
+      currency_count_day_ago = c.convert(1, combo_currency.get(), combo_new_currency.get(),date=date(date_ago.year, date_ago.month, date_ago.day))
+      currency_count_list.append(currency_count_day_ago)
+      average_currency = statistics.mean(currency_count_list)
+      if date_ago.day == today_count.day:
+         print("Average currency 1 month:", average_currency)
+         average_currency_a_month = "Average currency rate in 1 month: "+str("{:.3f}".format(average_currency))
+         label_average_currency_a_month.configure(text=average_currency_a_month)
 
 
 combo_currency.set('Search')
