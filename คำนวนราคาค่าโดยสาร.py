330e71a83ace2fcd5839e02cc8da90196e16dd23
Chomnan441 <159530699+Chomnan441@users.noreply.github.com>
2024-09-25 13:07:04 +0700

    Update คำนวนราคาค่าโดยสาร.py

--- คำนวนราคาค่าโดยสาร.py
+++ คำนวนราคาค่าโดยสาร.py
@@ -1,4 +1,5 @@
 distance = int(input())
+
 if distance == 0:
     cost = 0
 elif distance >0 and distance <=1:
@@ -10,9 +11,10 @@ elif distance > 10 and distance <= 20:
 elif distance > 20 and distance <= 40:
     cost = 35+(9*5.5)+(10*6.5)+(distance-20)*7.5
 elif distance > 40 and distance <= 60:
-    cost = 35+35+(9*5.5)+(10*6.5)+(10*7.5)+(distance-40)*8
+    cost = 35+(9*5.5)+(10*6.5)+(10*7.5)+(distance-40)*8
 elif distance > 60 and distance <= 80:
-    cost = 35+35+(9*5.5)+(10*6.5)+(10*7.5)+(10*8)+(distance-60)*9
+    cost = 35+(9*5.5)+(10*6.5)+(10*7.5)+(10*8)+(distance-60)*9
 elif distance > 80:
-    cost = 35+35+(9*5.5)+(10*6.5)+(10*7.5)+(10*8)+(10*9)+(distance-80)*10.5
+    cost = 35+(9*5.5)+(10*6.5)+(10*7.5)+(10*8)+(10*9)+(distance-80)*10.5
+  
 print(cost)
